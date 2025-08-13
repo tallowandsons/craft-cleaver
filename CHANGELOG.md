93cf1b17f2d140f12c5602041e2d9d3618036b1b
Michael Westwood <michael@honcho.agency>
2025-08-13 20:25:02 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for cleaver
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tallowandsons\cleaver\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * cleaver settings
+ */
+class Settings extends Model
+{
+}
