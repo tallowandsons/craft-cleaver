fd45aeaf3a4179c06212f067b1a0d988bbad1154
Michael Westwood <michael@honcho.agency>
2025-08-14 09:58:04 +0100

    chopconfig scaffolding

---
+++ src/models/ChopConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tallowandsons\cleaver\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Chop Config model
+ */
+class ChopConfig extends Model
+{
+    protected function defineRules(): array
+    {
+        return array_merge(parent::defineRules(), [
+            // ...
+        ]);
+    }
+}
